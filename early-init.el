da584e5227f04835de8b862a95d24d8231fc423a
Kamil Belter <kamil.belter@gmail.com>
2021-11-13 22:09:21 +0100

    Add early-init.el

--- early-init.el
+++ early-init.el
@@ -1,23 +1,26 @@
-;;; early-init.el -*- lexical-binding: t; -*-
+;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
 
 ;; Available since Emacs 27.1. It is run before:
 ;; - init.el is loaded
 ;; - UI initialization
 
-;; maximize gc size at startup to avoid gc slowing down startup
+;; To avoid garbage collection kick-in during startup
 (setq gc-cons-threshold most-positive-fixnum)
 
-;; unset file-name-handler-alist
+;; required for manual package initialization
+(setq package-enable-at-startup nil)
+
+;; Inhibit resizing frame
+(setq frame-inhibit-implied-resize t)
+
+;; file hanlders are not needed during startup
 (defvar file-name-handler-alist-original file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-;; prevent package.el loading 
-(setq package-enable-at-startup nil)
-
-;; unset site run file
+;; disable site run file
 (setq site-run-file nil)
 
-;; disable UI elements
+;; turn off UI elements: toolbar, menubar and vertical scrollbars
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
