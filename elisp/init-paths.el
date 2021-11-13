40e2749ce5e00451d5c4757e415c2399844dea93
Kamil Belter <kamil.belter@gmail.com>
2021-11-13 22:08:11 +0100

    Add init-paths

--- elisp/init-paths.el
+++ elisp/init-paths.el
@@ -1,20 +1,24 @@
 ;;; init-paths.el --- -*- lexical-binding: t -*-
 
-;; add folders to PATH
-(let (
-      (mypaths
-       '(
-         "C:/Python36"
-         "C:/Users/kamil/AppData/Roaming/npm"
-         "C:/Program Files (x86)/nodejs/"
-         "C:/Program Files/Git/bin"
-         "C:/ProgramData/chocolatey/bin"
-         "C:/Program Files (x86)/GnuWin32/bin"
-         ) )
-      )
+(setq exec-path (append '("C:/Program Files/Git/bin/"
+                          "C:/Program Files/Git/mingw64/bin/"
+                          "c:/Program Files/Git/usr/bin/"
+                          "C:/ProgramData/chocolatey/bin/"
+			  "C:/apps/jdk-16.0.2/bin" ) exec-path))
 
-  (setenv "PATH" (mapconcat 'identity mypaths ";") )
-  (setq exec-path (append mypaths (list "." exec-directory)) ))
+;; make sure Unix tools in from of 'exec-path'
+(let ((bash (executable-find "bash")))
+  (when bash
+    (push (file-name-directory bash) exec-path)))
+;; update PATH from exec-path
+(let ((path (mapcar 'file-truename
+                    (append exec-path
+                            (split-string (getenv "PATH") path-separator t)))))
+  (setenv "PATH" (mapconcat 'identity (delete-dups path) path-separator)))
 
+;; add 7zip to arc-mode (open archives in Emacs)
+(if (file-exists-p "C:/Program Files/7-Zip")
+    (add-to-list 'exec-path "C:/Program Files/7-Zip")
+  (message "7-Zip not found."))
 
 (provide 'init-paths)
