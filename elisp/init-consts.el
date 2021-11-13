48eafe81d38843054c5b0a6d999517aa13d6d6d2
Kamil Belter <kamil.belter@gmail.com>
2021-11-13 22:07:27 +0100

    Add init-consts.el

---
+++ elisp/init-consts.el
@@ -0,0 +1,16 @@
+;;; init-consts.el --- -*- lexical-binding: t -*-
+
+(setq user-full-name "Kamil Belter")
+(setq user-mail-address "kamil.belter@gmail.com")
+
+(defvar custom-gc-cons-threshold (if (display-graphic-p) 64000000 1600000)
+  "The default value to use for `gc-cons-threshold'. If you experience freezing,
+decrease this. If you experience stuttering, increase this.")
+
+(defvar custom-gc-cons-upper-limit (if (display-graphic-p) 512000000 128000000)
+  "The temporary value for `gc-cons-threshold' to defer it.")
+
+(defvar custom-gc-timer (run-with-idle-timer 10 t #'garbage-collect)
+  "Run garbarge collection when idle 10s.") 
+
+(provide 'init-consts)
