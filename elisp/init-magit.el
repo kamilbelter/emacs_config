a9125611df18d571181e4eb810870d981f160372
Kamil Belter <kamil.belter@gmail.com>
2021-11-21 15:10:21 +0100

    Add magit settings

---
+++ elisp/init-magit.el
@@ -0,0 +1,17 @@
+;;; init-magit.el --- -*- lexical-binding: t -*-
+
+(use-package magit
+  :straight t
+  :if (executable-find "git")
+  :bind
+  (("C-x g" . magit-status)
+   (:map magit-status-mode-map
+         ("M-RET" . magit-diff-visit-file-other-window)))
+  :config
+  (defun magit-log-follow-current-file ()
+    "A wrapper around `magit-log-buffer-file' with `--follow' argument."
+    (interactive)
+    (magit-log-buffer-file t)))
+;; -MagitPac
+
+(provide 'init-magit)
