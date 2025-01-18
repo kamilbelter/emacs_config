5f9fffee79401b0ebdbe2a12bf64b64aad2c3265
Kamil Belter <kamil.belter@gmail.com>
2025-01-18 23:56:48 +0100

    Update package manager
    Also rename init-pckg-mgr to init-straight.

---
+++ elisp/init-straight.el
@@ -0,0 +1,23 @@
+;;; init-straight.el --- -*- lexical-binding: t -*-
+
+(setq straight-log t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://radian-software.github.io/straight.el/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(provide 'init-straight)
