02620e5cc67e62b46853c9c491925365f4691963
Kamil Belter <kamil.belter@gmail.com>
2025-01-21 22:07:35 +0100

    Complete orgmode

--- elisp/init-orgmode.el
+++ elisp/init-orgmode.el
@@ -1,5 +1,19 @@
 ;;; init-orgmode.el --- -*- lexical-binding: t -*-
 
+(use-package org
+  :straight t ; use latests-greatest
+  :config
+  (setq org-M-RET-may-split-line '((default . nil)))
+  (setq org-insert-heading-respect-content t)
+  (setq org-log-done 'time)
+  (setq org-log-into-drawer t)
+
+  (setq org-directory "~/Documents/org/")
+  (setq org-agenda-files (list org-directory))
+
+  (setq org-todo-keywords
+	'((sequence "TODO(t)" "ONHOLD(h!)" "|" "CANCEL(c!)" "DONE(d!)"))))
+
 (use-package org-modern
   :straight t
   :after org
