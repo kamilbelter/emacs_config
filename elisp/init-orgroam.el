b8ab30cfc49757a1cee6377f59b3d54b3a1c2861
Kamil Belter <kamil.belter@gmail.com>
2025-01-22 07:45:04 +0100

    Add org-roam templates & allow spaces in node names

--- elisp/init-orgroam.el
+++ elisp/init-orgroam.el
@@ -5,6 +5,19 @@
   :custom
   (org-roam-directory (file-truename "~/Documents/RoamNotes"))
   (org-roam-completion-everywhere t)
+  (org-roam-capture-templates
+   '(("d" "default" plain
+      "%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+      :unnarrowed t)
+     ("b" "book notes" plain
+      "\n* Source\n\nAuthor: %^{Author}\nTitle: ${title}\nYear: %^{Year}\n\n* Summary\n\n%?"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n")
+      :unnarrowed t)
+     ("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: Project")
+      :unnarrowed t)
+     ))
   :bind (("C-c n l" . org-roam-buffer-toggle)
 	 ("C-c n f" . org-roam-node-find)
 	 ("C-c n g" . org-roam-graph)
@@ -16,6 +29,8 @@
   (setq org-roam-dailies-directory "journal/")
   (org-roam-db-autosync-mode)
   (org-roam-setup))
+
+(define-key minibuffer-local-completion-map (kbd "SPC") 'self-insert-command)
 
 (use-package emacsql
   :straight (emacsql :type git :host github :repo "magit/emacsql"))
